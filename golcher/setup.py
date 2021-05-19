f5ec187d520f08bc5a8e1fac72c5b79740bb5235
LyubomyrIvanitskiy <lubomyr.ivanitskiy@gmail.com>
2021-05-19 10:21:00 +0300

    Added setup.py

---
+++ golcher/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='golcher',
+    version='0.95',
+    packages=[''],
+    url='https://github.com/LubomyrIvanitskiy/golcher',
+    license='MIT',
+    author='liubomyr.ivanitskyi',
+    author_email='lubomyr.ivanitskiy@gmail.com',
+    description='Suffix Tree Lib'
+)
